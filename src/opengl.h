a1e71a14721c920a3b45e67314f38abee77c53fb
Roger Gee <rpg11a@acu.edu>
2016-02-19 16:25:06 -0600

    Add OpenGL platform wrapper

---
+++ src/opengl.h
@@ -0,0 +1,14 @@
+// opengl.h - trafficsim
+#ifndef TRAFFICSIM_OPENGL_H
+#define TRAFFICSIM_OPENGL_H
+
+// include OpenGL platform headers
+#ifdef TRAFFICSIM_GTKX
+#include <GL/gl.h>
+
+#elif defined(TRAFFICSIM_WIN32)
+#include <gl/GL.h>
+
+#endif
+
+#endif
